7ba9faf6db6bc9436c22e60bfb35c8eb6ae6e7ea
YagoAlmeidaDev <32191996+YagoAlmeidaDev@users.noreply.github.com>
2022-05-16 14:49:12 +0200

    Primeros pasos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+
+  const [values, setValues] = React.useState({
+    email: "",
+    password: "",
+  });
+  function handleSubmit(evt) {
+    /*
+      Previene el comportamiento default de los
+      formularios el cual recarga el sitio
+    */
+    evt.preventDefault();
+    // Aquí puedes usar values para enviar la información
+  }
+  function handleChange(evt) {
+    /*
+      evt.target es el elemento que ejecuto el evento
+      name identifica el input y value describe el valor actual
+    */
+    const { target } = evt;
+    const { name, value } = target;
+    /*
+      Este snippet:
+      1. Clona el estado actual
+      2. Reemplaza solo el valor del
+         input que ejecutó el evento
+    */
+    const newValues = {
+      ...values,
+      [name]: value,
+    };
+    // Sincroniza el estado de nuevo
+    setValues(newValues);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit} className="App">
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        value={values.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Sign Up</button>
+    </form>
   );
 }
 
