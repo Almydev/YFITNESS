66b21230cf145dcb09df2fef69bfea10acd89631
YagoAlmeidaDev <yagoalmeida.dev@gmail.com>
2022-07-05 00:51:59 +0200

    ListadoEjercicios + ventanas modales

--- js/movimiento.js
+++ js/movimiento.js
@@ -2,7 +2,27 @@ $(document).ready(e);
 
 function e(){
     $("#ejercicios").click(function(){
+        $("#contenedor").load("/html/ejercicios.html");
+    });
+
+    $("#rutina").click(function(){
         $("#contenedor").load("/html/mirutina.html");
+    });
+    $("#calendario").click(function(){
+        $("#contenedor").load("/html/calendario.html");
+    });
+    $("#ajustes").click(function(){
+        $("#contenedor").load("/html/ajustes.html");
+    });
+    $("#soporte").click(function(){
+        $("#contenedor").load("/html/soporteyfitness.html");
     })
+
+
+
+    $("#bodybuilding").click(function(){
+        $("body").load("/php/listadoEjercicios.php");
+    })
+
 }
     
